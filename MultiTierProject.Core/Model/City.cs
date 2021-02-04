eae7005e8fd5e4c7b8f227809e4fba42f57edc2f
Burak BOZ <boz-burak-trash@hotmail.com>
2021-02-04 22:11:05 +0300

    Add project files.

---
+++ MultiTierProject.Core/Model/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiTierProject.Core.Model
+{
+    internal class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RegionId { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual Region Region { get; set; }
+    }
+}
---
+++ MultiTierProject.Core/Model/Region.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MultiTierProject.Core.Model
+{
+    internal class Region
+    {
+        public Region()
+        {
+            Cities = new Collection<City>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<City> Cities { get; set; }
+    }
+}
