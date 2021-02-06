3383ba0cb2054dd6f4d3c82838ce56f539d4aa90
Burak BOZ <boz-burak-trash@hotmail.com>
2021-02-06 13:47:37 +0300

    created seeds

--- MultiTierProject.Data/MultiTierDbContext.cs
+++ MultiTierProject.Data/MultiTierDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MultiTierProject.Core.Models;
 using MultiTierProject.Data.Configurations;
+using MultiTierProject.Data.Seeds;
 
 namespace MultiTierProject.Data
 {
@@ -13,6 +14,9 @@ namespace MultiTierProject.Data
         {
             modelBuilder.ApplyConfiguration(new CityConfiguration());
             modelBuilder.ApplyConfiguration(new RegionConfiguration());
+
+            modelBuilder.ApplyConfiguration(new CitySeed(new int[] { 1, 2, 3 }));
+            modelBuilder.ApplyConfiguration(new RegionSeed(new int[] { 1, 2, 3 }));
         }
     }
 }
---
+++ MultiTierProject.Data/Seeds/CitySeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiTierProject.Core.Models;
+
+namespace MultiTierProject.Data.Seeds
+{
+    class CitySeed : IEntityTypeConfiguration<City>
+    {
+        private readonly int[] _ids;
+
+        public CitySeed(int[] ids)
+        {
+            _ids = ids;
+        }
+
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasData
+            (
+                new City { Id = 1, Name = "İstanbul", RegionId = _ids[0] },
+                new City { Id = 2, Name = "İzmir", RegionId = _ids[1] },
+                new City { Id = 3, Name = "Ankara", RegionId = _ids[2] }
+            );
+        }
+    }
+}
