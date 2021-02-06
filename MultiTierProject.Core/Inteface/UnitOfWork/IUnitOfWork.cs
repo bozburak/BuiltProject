9564b14318b8862ee51140a61db3489d35f03e09
Burak BOZ <boz-burak-trash@hotmail.com>
2021-02-06 12:34:27 +0300

    created unit of work interface

---
+++ MultiTierProject.Core/Inteface/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace MultiTierProject.Core.Inteface.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
